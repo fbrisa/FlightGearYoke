c16f4cd98a9f3cfdbb6fee647a401cf0b79ebc2c
francesco <fbrisa@gmail.com>
2015-09-29 18:25:02 +0200

    connection activity 1

---
+++ app/src/main/java/bfa/fgfs/flightgearyoke/ConnectionActivityFragment.java
@@ -0,0 +1,22 @@
+package bfa.fgfs.flightgearyoke;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ConnectionActivityFragment extends Fragment {
+
+    public ConnectionActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_connection, container, false);
+    }
+}
